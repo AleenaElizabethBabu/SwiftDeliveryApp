71f7fb46315f74e70815ace26e2bec2059b232fa
AleenaElizabethBabu <165337917+AleenaElizabethBabu@users.noreply.github.com>
2024-11-17 18:18:26 -0500

    homepage

--- SwiftDeliveryApp/HomePage.xaml.cs
+++ SwiftDeliveryApp/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
 
 namespace SwiftDeliveryApp
 {
@@ -7,6 +8,39 @@ namespace SwiftDeliveryApp
         public HomePage()
         {
             InitializeComponent();
+            LoadRestaurants();
+        }
+
+        private void LoadRestaurants()
+        {
+            // Sample data for restaurants
+            var restaurants = new List<Restaurant>
+            {
+                new Restaurant
+                {
+                    Name = "Pasta Palace",
+                    Cuisine = "Italian",
+                    Rating = "⭐️⭐️⭐️⭐️",
+                    Image = "pasta_palace.png" // Place this image in the Resources/Drawable folder
+                },
+                new Restaurant
+                {
+                    Name = "Sushi Spot",
+                    Cuisine = "Japanese",
+                    Rating = "⭐️⭐️⭐️⭐️⭐️",
+                    Image = "sushi_spot.png" // Place this image in the Resources/Drawable folder
+                },
+                new Restaurant
+                {
+                    Name = "Curry Corner",
+                    Cuisine = "Indian",
+                    Rating = "⭐️⭐️⭐️⭐️",
+                    Image = "curry_corner.png" // Place this image in the Resources/Drawable folder
+                }
+            };
+
+            // Bind the data to the CollectionView
+            RestaurantCollectionView.ItemsSource = restaurants;
         }
     }
 }
---
+++ SwiftDeliveryApp/Resturant.cs
@@ -0,0 +1,10 @@
+﻿namespace SwiftDeliveryApp
+{
+    public class Restaurant
+    {
+        public string Name { get; set; }
+        public string Cuisine { get; set; }
+        public string Rating { get; set; }
+        public string Image { get; set; }
+    }
+}
